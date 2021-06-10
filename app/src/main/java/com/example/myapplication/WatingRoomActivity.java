2c8b94b71d575f9600421948c1443eec7315af05
jangtang2 <atoa04@naver,com>
2021-06-10 22:20:23 +0900

    [android] 대기방 버튼 연결

--- app/src/main/java/com/example/myapplication/WatingRoomActivity.java
+++ app/src/main/java/com/example/myapplication/WatingRoomActivity.java
@@ -2,13 +2,36 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WatingRoomActivity extends AppCompatActivity {
+
+    Button connectButton, startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wating_room);
+
+        connectButton = findViewById(R.id.userConnection);
+        connectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WatingRoomActivity.this, UserConnectionActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        startButton = findViewById(R.id.startGame);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WatingRoomActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
