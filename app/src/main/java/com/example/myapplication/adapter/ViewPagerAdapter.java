cb2f30fbfbb918e3382cca845d0f118ef8b96163
jangtang2 <atoa04@naver,com>
2021-06-10 21:27:40 +0900

    [android] ViewPagerAdapter 생성

---
+++ app/src/main/java/com/example/myapplication/adapter/ViewPagerAdapter.java
@@ -0,0 +1,8 @@
+package com.example.myapplication.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class ViewPagerAdapter{}
