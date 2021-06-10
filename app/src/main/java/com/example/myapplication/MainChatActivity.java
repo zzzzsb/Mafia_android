0f5560b227c19399a2593b9e5409034c294e30a6
jangtang2 <atoa04@naver,com>
2021-06-10 13:29:05 +0900

    [android] 채팅방 접속상태 버튼 연결

--- app/src/main/java/com/example/myapplication/MainChatActivity.java
+++ app/src/main/java/com/example/myapplication/MainChatActivity.java
@@ -1,16 +1,31 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import com.example.myapplication.databinding.ActivityLoginBinding;
 
 public class MainChatActivity extends AppCompatActivity {
+    Button connectButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_chat);
+
+        connectButton = findViewById(R.id.connectButton);
+        connectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainChatActivity.this, UserConnectionActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void clickSend(View view){
