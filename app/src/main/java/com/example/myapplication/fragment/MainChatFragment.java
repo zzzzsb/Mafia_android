7f0cc3676176fd401aa543540b5df16773ed01c3
sxxdiodev <ji12subin@gmail.com>
2021-06-10 20:00:27 +0900

    프래그먼트 0

--- app/src/main/java/com/example/myapplication/fragment/MainChatFragment.java
+++ app/src/main/java/com/example/myapplication/fragment/MainChatFragment.java
@@ -4,19 +4,41 @@ import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.example.myapplication.MainActivity;
 import com.example.myapplication.R;
+import com.example.myapplication.chat.ChatAdapter;
+import com.example.myapplication.chat.MessageItem;
 import com.example.myapplication.stomp.StompAPI;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainChatFragment extends Fragment {
 
+    View view;
+
     EditText et;
+    TextView roleTextView;
+
+    List<MessageItem> messageItems = new ArrayList<>();
+    ChatAdapter adapter;
+    RecyclerView recyclerView;
+    Button sendBtn;
+
+    private String userId;
+
     String nickname;
     StompAPI stompAPI = new StompAPI();
 
@@ -24,11 +46,44 @@ public class MainChatFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_chat, container, false);
+        view = inflater.inflate(R.layout.fragment_main_chat, container, false);
 
+        et= view.findViewById(R.id.et);
+        sendBtn = view.findViewById(R.id.sendBtn);
+        recyclerView = view.findViewById(R.id.chatRecycle);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+        userId = getActivity().getIntent().getStringExtra("userId");
+
+        this.nickname = getActivity().getIntent().getStringExtra("nickname");
+        setAdapter(recyclerView);
+
+        stompAPI.setAllChatListener(new StompAPI.ChatListener() {
+            @Override
+            public void onMessageReceived(MessageItem messageItem) {
+                //System.out.println(messageItems.size());
+                messageItems.add(messageItem);
+
+                System.out.println("LISTSIZE: " + messageItems.size());
+                adapter.notifyDataSetChanged();
+                recyclerView.scrollToPosition(messageItems.size()-1);
+
+            }
+        });
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickSend(view);
+            }
+        });
+
+        stompAPI.initStomp();
+        stompAPI.onConnected(userId);
+
+        return view;
     }
 
-    public void clickSend(View view){//firebase DB에 저장할 값들(네임, 메세지, 시간)
+    public void clickSend(View view){
         String message= et.getText().toString();
         stompAPI.sendMessage(nickname, message);
 
@@ -41,5 +96,9 @@ public class MainChatFragment extends Fragment {
 
     }
 
+    private void setAdapter(RecyclerView recyclerView) {
+        adapter = new ChatAdapter(messageItems , nickname);
+        recyclerView.setAdapter(adapter);
+    }
 
 }
